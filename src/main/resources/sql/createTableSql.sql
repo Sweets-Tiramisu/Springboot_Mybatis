27aa07af3bba796cb6ad3f101bb932dfa5f78311
qiya <loveonly@aliyun.com>
2021-08-10 16:02:32 +0800

    新增建表语句

---
+++ src/main/resources/sql/createTableSql.sql
@@ -0,0 +1,11 @@
+
+create table test.user
+(
+    realName varchar(20) null,
+    passWord varchar(20) null,
+    userName varchar(20) null,
+    id       int         not null
+        primary key
+);
+
+
